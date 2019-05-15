7482af10995afdd20d657c360fe0b3289f48b9e6
sshegokar <ssshegokar21@gmail.com>
2019-05-15 17:47:52 +0530

    Modified linear regression functions for saving and loding model

--- Demo/PythonPrograms/BikesVsTempSimpleLinearRegression.py
+++ Demo/PythonPrograms/BikesVsTempSimpleLinearRegression.py
@@ -1,11 +1,20 @@
 # Importing libraries
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from matplotlib import pyplot as plt
 
-# Importing dataSet
+# Importing dataSet and splitting dataSet into 2 csv files
 dataSet = pd.read_csv('../inputFiles/bike_sharing.csv')
+df_training = dataSet.sample(frac=0.8)
+df_test = pd.concat([dataSet, df_training]).drop_duplicates(keep=False)
+
+df_training.to_csv('../inputFiles/training_data.csv', header=True, index=None)
+df_test.to_csv('../inputFiles/test_data.csv', header=True, index=None)
+
+dataSet = pd.read_csv('../inputFiles/training_data.csv')
 x_index = dataSet.columns.get_loc("temp")
 y_index = dataSet.columns.get_loc("cnt")
 x = dataSet.iloc[:, x_index:(x_index+1)]
@@ -21,8 +30,14 @@ if y['cnt'].isnull().sum() > 0:
 else:
     print("Null values are not present")
 
+# Feature Scaling
+sc_x = StandardScaler()
+x = sc_x.fit_transform(x)
+
 # Splitting data set into training and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+print(x_train.shape)
+print(x_test.shape)
 
 # Fitting training set into linear model
 regression = LinearRegression()
@@ -31,18 +46,22 @@ regression.fit(x_train, y_train)
 # Predicting values
 y_pred = regression.predict(x_test)
 
-# Plotting graph for training set
-plt.scatter(x_train, y_train, c='r')
-plt.plot(x_train, regression.predict(x_train), c='b')
-plt.title("Temperature Vs Bikes for training set")
-plt.xlabel("Temperature")
-plt.ylabel("Bikes")
-plt.show()
+# Finding accuracy using r2_score
+accuracy = r2_score(y_test, y_pred)
+print("Accuracy ", accuracy)
 
-# Plotting graph for test set
-plt.scatter(x_test, y_test, c='r')
-plt.plot(x_train, regression.predict(x_train), c='b')
-plt.title("Temperature Vs Bikes for test set")
-plt.xlabel("Temperature")
-plt.ylabel("Bikes")
-plt.show()
+# # Plotting graph for training set
+# plt.scatter(x_train, y_train, c='r')
+# plt.plot(x_train, regression.predict(x_train), c='b')
+# plt.title("Temperature Vs Bikes for training set")
+# plt.xlabel("Temperature")
+# plt.ylabel("Bikes")
+# plt.show()
+#
+# # Plotting graph for test set
+# plt.scatter(x_test, y_test, c='r')
+# plt.plot(x_train, regression.predict(x_train), c='b')
+# plt.title("Temperature Vs Bikes for test set")
+# plt.xlabel("Temperature")
+# plt.ylabel("Bikes")
+# plt.show()
--- Demo/PythonPrograms/SalaryVsExperienceLinearRegression.py
+++ Demo/PythonPrograms/SalaryVsExperienceLinearRegression.py
@@ -2,10 +2,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from matplotlib import pyplot as plt
+from sklearn.metrics import r2_score
+import pickle
 
-# Importing data-set
+# Importing data-set and converting into two csv Files
 dataSet = pd.read_csv('../inputFiles/Salary_Data.csv')
+df_training = dataSet.sample(frac=0.8)
+df_test = pd.concat([dataSet, df_training]).drop_duplicates(keep=False)
+
+df_training.to_csv('../inputFiles/training_data.csv', header=True, index=None)
+df_test.to_csv('../inputFiles/test_data.csv', header=True, index=None)
+
+dataSet = pd.read_csv('../inputFiles/training_data.csv')
 x = dataSet.iloc[:, 0:-1]
 y = dataSet.iloc[:, -1:0:-1]
 
@@ -20,7 +28,7 @@ else:
     print("Null values are not present")
 
 # Splitting data-set into training and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 # Fitting training set
 regression = LinearRegression()
@@ -29,18 +37,38 @@ regression.fit(x_train, y_train)
 # Predicting test set
 y_pred = regression.predict(x_test)
 
-# Plotting training set result into graphs
-plt.scatter(x_train, y_train, c='r')
-plt.plot(x_train, regression.predict(x_train), c='b')
-plt.title("Experience Vs Salary for training set")
-plt.xlabel("Experience")
-plt.ylabel("Salary")
-plt.show()
+# Calculating accuracy of model
+accuracy = r2_score(y_test, y_pred)
+print("Accuracy of model ", accuracy)
 
-# Plotting test set result into graphs
-plt.scatter(x_test, y_test, c='r')
-plt.plot(x_train, regression.predict(x_train), c='b')
-plt.title("Experience Vs Salary for test set")
-plt.xlabel("Experience")
-plt.ylabel("Salary")
-plt.show()
+# Creating and saving pickle model
+pkl_fileName = '../inputFiles/LinearRegression.pkl'
+linearRegressionPklModel = open(pkl_fileName, 'wb')
+pickle.dump(regression, linearRegressionPklModel)
+linearRegressionPklModel.close()
+
+# Loading pickle model to predict y data from test_data.csv
+linearRegressionPklModel = open(pkl_fileName, 'rb')
+regressionModelPkl = pickle.load(linearRegressionPklModel)
+
+# # Use pickle's regression model to Predict y values
+# dataSet_testData = pd.read_csv('')
+# y_pred_pkl = regressionModelPkl()
+
+
+
+# # Plotting training set result into graphs
+# plt.scatter(x_train, y_train, c='r')
+# plt.plot(x_train, regression.predict(x_train), c='b')
+# plt.title("Experience Vs Salary for training set")
+# plt.xlabel("Experience")
+# plt.ylabel("Salary")
+# plt.show()
+#
+# # Plotting test set result into graphs
+# plt.scatter(x_test, y_test, c='r')
+# plt.plot(x_train, regression.predict(x_train), c='b')
+# plt.title("Experience Vs Salary for test set")
+# plt.xlabel("Experience")
+# plt.ylabel("Salary")
+# plt.show()
