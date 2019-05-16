85cb8976a67cba0dcea6ec94dbbda362aae3b09e
sshegokar <ssshegokar21@gmail.com>
2019-05-16 15:55:18 +0530

    Added check, if accuracy is greater than 0.8 then only saving model code will get executed

--- Demo/PythonPrograms/SalaryVsExperienceDecisionRegression.py
+++ Demo/PythonPrograms/SalaryVsExperienceDecisionRegression.py
@@ -5,6 +5,8 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 import pickle
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Importing dataset and splitting dataset into 2 different csv files
 dataSet = pd.read_csv('../inputFiles/Position_Salaries.csv')
@@ -50,22 +52,27 @@ y_pred = regression.predict(x_test)
 accuracy = r2_score(y_test, y_pred)
 print("Accuracy from model is : ", accuracy)
 
-# # Dumping model into pickle
-# file_name = '../inputFiles/DecisionTreeRegression.pkl'
-# pkl_file = open(file_name, 'wb')
-# model = pickle.dump(regression, pkl_file)
-#
-# # Loading pickle model to predict data from test_data.csv file
-# pkl_file = open(file_name, 'rb')
-# model_pkl = pickle.load(pkl_file)
-#
-# dataSet_testdata = pd.read_csv('../inputFiles/test_data.csv')
-#
-# x_testdata = dataSet_testdata.iloc[:, x_index:(x_index+1)]
-# y_testdata = dataSet_testdata.iloc[:, y_index:(y_index+1)]
-#
-# y_pred_pkl = model_pkl.predict(x_testdata)
-#
-# # Calculating accuracy from pickle model
-# accuracy_pk = r2_score(y_testdata, y_pred_pkl)
-# print("Accuracy by pickle model ", accuracy_pk)
+# Dumping model into pickle
+if accuracy > 0.8:
+    file_name = '../inputFiles/DecisionTreeRegression.pkl'
+    pkl_file = open(file_name, 'wb')
+    model = pickle.dump(regression, pkl_file)
+
+    # Loading pickle model to predict data from test_data.csv file
+    pkl_file = open(file_name, 'rb')
+    model_pkl = pickle.load(pkl_file)
+
+    dataSet_testdata = pd.read_csv('../inputFiles/test_data.csv')
+
+    x_testdata = dataSet_testdata.iloc[:, x_index:(x_index+1)]
+    y_testdata = dataSet_testdata.iloc[:, y_index:(y_index+1)]
+
+    y_pred_pkl = model_pkl.predict(x_testdata)
+
+    # Calculating accuracy from pickle model
+    accuracy_pk = r2_score(y_testdata, y_pred_pkl)
+    print("Accuracy by pickle model ", accuracy_pk)
+
+
+
+
