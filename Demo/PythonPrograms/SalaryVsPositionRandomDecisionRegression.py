3dc42ea49c0e521722a23c251426a620d6d6e1cf
sanjanasunilpatil <sanjanapatil0894@gmail.com>
2019-05-16 22:14:36 +0530

    Built a Random forest regression model to predict salary vs position for given dataset

---
+++ Demo/PythonPrograms/SalaryVsPositionRandomDecisionRegression.py
@@ -0,0 +1,76 @@
+# Importing libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+# Importing dataset and splitting dataset into 2 different csv files
+dataSet = pd.read_csv('../inputFiles/Position_Salaries.csv')
+
+df_training = dataSet.sample(frac=0.7)
+df_test = pd.concat([dataSet, df_training]).drop_duplicates(keep=False)
+
+df_training.to_csv('../inputFiles/training_data.csv', header=True, index=None)
+df_test.to_csv('../inputFiles/test_data.csv', header=True, index=None)
+
+dataSet = pd.read_csv('../inputFiles/training_data.csv')
+x_index = dataSet.columns.get_loc("Position")
+y_index = dataSet.columns.get_loc("Salary")
+
+x = dataSet.iloc[:, x_index:(x_index+1)]
+y = dataSet.iloc[:, y_index:(y_index+1)]
+
+# Handling categorical data
+postitions = pd.get_dummies(x['Position'])
+x = x.drop('Position', axis=1)
+x = pd.concat([x, postitions], axis=1)
+
+# Checking for null values
+if y['Salary'].isnull().sum() > 0:
+    print("Taking care of null values of Salary column")
+    y = y.fillna(y.mean())
+
+# Feature scaling
+scX = StandardScaler()
+x = scX.fit_transform(x)
+
+# Splitting data into training and test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+# # Fitting training data into model
+regression = RandomForestRegressor(n_estimators=10, random_state=0)
+regression.fit(x_train, y_train.values.ravel())
+
+# Using model to predict test data
+y_pred = regression.predict(x_test)
+
+# Calculating accuracy
+accuracy = r2_score(y_test, y_pred)
+print("Accuracy from model is : ", accuracy)
+
+# Dumping model into pickle
+if accuracy > 0.8:
+    file_name = '../inputFiles/RandomForestRegression.pkl'
+    pkl_file = open(file_name, 'wb')
+    model = pickle.dump(regression, pkl_file)
+
+    # Loading pickle model to predict data from test_data.csv file
+    pkl_file = open(file_name, 'rb')
+    model_pkl = pickle.load(pkl_file)
+
+    dataSet_testdata = pd.read_csv('../inputFiles/test_data.csv')
+
+    x_testdata = dataSet_testdata.iloc[:, x_index:(x_index+1)]
+    y_testdata = dataSet_testdata.iloc[:, y_index:(y_index+1)]
+
+    y_pred_pkl = model_pkl.predict(x_testdata)
+
+    # Calculating accuracy from pickle model
+    accuracy_pk = r2_score(y_testdata, y_pred_pkl)
+    print("Accuracy by pickle model ", accuracy_pk)
+
+
+
+
