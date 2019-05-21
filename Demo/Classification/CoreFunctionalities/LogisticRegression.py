d0daba04e18cd5779f1914baefe196f4563c11bb
sshegokar <ssshegokar21@gmail.com>
2019-05-21 16:43:30 +0530

    Called one more function to load pickle model

--- Demo/Classification/CoreFunctionalities/LogisticRegression.py
+++ Demo/Classification/CoreFunctionalities/LogisticRegression.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 import warnings
 from sklearn.metrics import accuracy_score
 from Demo.Classification.CoreFunctionalities.ModelSerialization import ModelSerialization
+from Demo.Classification.ModelUsage.ModelDeserialization import ModelDeserialization
 
 
 class ImplementAlgorithm(MLAlgorithmAbstract):
@@ -13,15 +14,18 @@ class ImplementAlgorithm(MLAlgorithmAbstract):
         classifier = LogisticRegression()
 
         result = MLAlgorithmAbstract.handle_dataset(self, filePath, yColumn, xColumnNumerical, xColumnCategorical)
-        classifier.fit(result[0], result[2].values.ravel())
+        classifier.fit(result[0][0], result[0][2].values.ravel())
 
-        y_pred = classifier.predict(result[1])
+        y_pred = classifier.predict(result[0][1])
 
-        accuracy = accuracy_score(result[3], y_pred)
+        accuracy = accuracy_score(result[0][3], y_pred)
+
+        print("Acurracy of training data", accuracy)
 
         if accuracy > 0.8:
             ModelSerialization.saveModel(self, classifier)
+            ModelDeserialization.loadModel(self, '../inputFiles/LogisticRegression.pkl', yColumn, result[1])
 
 
-i = ImplementAlgorithm()
-i.createAndUseModel('../inputFiles/Social_Network_Ads.csv', 'Purchased', ['Age', 'EstimatedSalary'], [])
+implement = ImplementAlgorithm()
+implement.createAndUseModel('../inputFiles/Social_Network_Ads.csv', 'Purchased', ['Age', 'EstimatedSalary'], [])
