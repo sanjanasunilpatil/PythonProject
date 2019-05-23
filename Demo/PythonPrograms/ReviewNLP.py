1ce594dd3ad62e01777105ba58e416074661b548
sanjanasunilpatil <sanjanapatil0894@gmail.com>
2019-05-23 21:42:59 +0530

    Updated NLP program to calculate accuracy of model

--- Demo/PythonPrograms/ReviewNLP.py
+++ Demo/PythonPrograms/ReviewNLP.py
@@ -5,6 +5,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 # Importing dataset
 dataset = pd.read_csv('../inputFiles/Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
@@ -29,5 +32,19 @@ cv = CountVectorizer(max_features=1500)
 x = cv.fit_transform(updated_review).toarray()
 y_index = dataset.columns.get_loc("Liked")
 y = dataset.iloc[:, y_index:(y_index+1)]
+
+# Split data into training and test dataset
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# Fit data into model
+classifier = DecisionTreeClassifier()
+classifier.fit(x_train, y_train)
+
+# Predict data using model
+y_pred = classifier.predict(x_test)
+
+# Calculate accuracy of model
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
 
 
